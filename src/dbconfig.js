652be7a6e2d924599a343404a464000214906523
AndresFVargasV <afvasquez@uninorte.edu.co>
2023-10-10 00:58:48 -0500

    database added

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -1,10 +1,10 @@
 const sql = require('mssql');
 
 const config = {
-    server: 'adminacor.database.windows.net',
-    database: 'crud_microservices',
-    user: 'adminacor',
-    password: '12345',
+    server: '',
+    database: '',
+    user: '',
+    password: '',
     options: {
         encrypt: true // Use this if you're on Windows Azure
     }
